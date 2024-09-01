7b58f98dcfd71eeedce126e2da8c062db46528df
thequang <nguyenthequangdev@gmail.com>
2024-09-01 21:30:28 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+    // Thư viện vừa cài npm install express
+const app = express()
+    // Là 1 function
+    // Trả về 1 đối tượng, đại diện cho ứng dụng nodejs
+const port = 3000
+    // Cổng
+app.get('/', (req, res) => {
+    // '/' định nghĩa tuyến đường mặc định (trang chủ)
+  res.send('Hello World!')
+})
+
+// Địa chỉ ip 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on locallhost:${port}`)
+})
