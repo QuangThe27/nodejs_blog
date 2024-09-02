d7e98324e3a60988f6740d55b9ce236e20348251
thequang <nguyenthequangdev@gmail.com>
2024-09-02 23:27:43 +0700

    Cài đặt thư viện Morgan

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
     // Thư viện vừa cài npm install express
+const morgan = require('morgan')
 const app = express()
     // Là 1 function
     // Trả về 1 đối tượng, đại diện cho ứng dụng nodejs
 const port = 3000
     // Cổng
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
     // '/' định nghĩa tuyến đường mặc định (trang chủ)
   res.send('Hello World!')
